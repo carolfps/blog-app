6441dd53027036f8aef3ad0913069773ee1ec9b5
carolfps <37373110+carolfps@users.noreply.github.com>
2020-09-13 11:58:36 -0300

    Register new category on mongodb

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const admin = require('./routes/admin');
 const path = require('path');
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const app = express();
 
 //Configs
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 //Handlebars
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+//Mongoose
+mongoose.connect('mongodb://localhost/blogapp', { useNewUrlParser:true, useUnifiedTopology: true}).then(()=>{
+    console.log('Mongo live')
+}).catch((err) => {
+    console.log(`Unable to connect Mongo: ${err}`)
+})
 //Public
 app.use(express.static(path.join(__dirname,"public")));
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Category')
+const Category = mongoose.model('categories');
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -16,6 +19,19 @@ router.get('/categories', (req,res) => {
 
 router.get('/categories/add', (req,res) => {
     res.render('admin/addcategories')
+})
+
+router.post('/categories/new', (req,res) => {
+    const newCategory = {
+        name: req.body.name,
+        slug: req.body.slug
+    }
+
+    new Category(newCategory).save().then(() => {
+        console.log('New category created')
+    }).catch((err) => {
+        console.log(`Unable to create new category: ${err}`)
+    })
 })
 
 module.exports = router;
