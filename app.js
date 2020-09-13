c0191ad682fee71039e2dd6552228821d4640a7b
carolfps <37373110+carolfps@users.noreply.github.com>
2020-09-13 11:00:53 -0300

    Routes

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const admin = require('./routes/admin');
+const path = require('path');
 //const mongoose = require('mongoose')
 const app = express();
 
-//Config
+//Configs
 //Body Parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 //Handlebars
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+//Public
+app.use(express.static(path.join(__dirname,"public")));
 
+//Routes
+app.use('/admin', admin)
 
 const PORT = 8081;
 app.listen(PORT, ()=>{
