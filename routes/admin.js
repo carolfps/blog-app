06afcc4f6304b2607e392d5c06ec4af4141df823
carolfps <37373110+carolfps@users.noreply.github.com>
2020-09-13 13:19:19 -0300

    Show list of categories

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,24 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-require('../models/Category')
+require('../models/Category');
 const Category = mongoose.model('categories');
 
 router.get('/', (req, res) => {
-    res.render('admin/index')
+    res.render('admin/index');
 })
 
 router.get('/posts', (req, res) => {
-    res.send('Blog')
+    res.send('Blog');
 })
 
 router.get('/categories', (req,res) => {
-    res.render('admin/categories')
+    Category.find().sort({name: 'asc'}).lean().then((categories) => {
+        res.render('admin/categories', {categories: categories});
+    }).catch((err) => {
+        req.flash('error_msg', 'Unable to list categories');
+        res.redirect('/admin');
+    })
 })
 
 router.get('/categories/add', (req,res) => {
@@ -25,19 +30,19 @@ router.post('/categories/new', (req,res) => {
     let errors = [];
 
     if( !req.body.name || req.body.name == undefined || req.body.name == null){
-        errors.push({text: 'Invalid category name'})
+        errors.push({text: 'Invalid category name'});
     }
 
     if( !req.body.slug || req.body.slug == undefined || req.body.name == null){
-        errors.push({text: 'Invalid slug'})
+        errors.push({text: 'Invalid slug'});
     }
 
     if( req.body.name.length < 3 ){
-        errors.push({text: 'Category name too small'})
+        errors.push({text: 'Category name too small'});
     }
 
     if( errors.length > 0){
-        res.render('admin/addcategories', {errors: errors})
+        res.render('admin/addcategories', {errors: errors});
     } else{
         const newCategory = {
             name: req.body.name,
@@ -45,11 +50,11 @@ router.post('/categories/new', (req,res) => {
         }
 
         new Category(newCategory).save().then(() => {
-            req.flash('success_msg', 'New category created!')
-            res.redirect('/admin/categories')
+            req.flash('success_msg', 'New category created!');
+            res.redirect('/admin/categories');
         }).catch((err) => {
-            req.flash('error_msg', 'Unable to create new category')
-            console.log(`Unable to create new category: ${err}`)
+            req.flash('error_msg', 'Unable to create new category');
+            console.log(`Unable to create new category: ${err}`);
         })
     }
     
